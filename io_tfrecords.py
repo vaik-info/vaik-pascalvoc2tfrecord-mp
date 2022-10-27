b545bf6c826db64b68289da51c9dfa120959dfb4
vaik-info <info@vaikobo.com>
2022-10-27 11:47:25 +0900

    first commit

---
+++ io_tfrecords.py
@@ -0,0 +1,81 @@
+import os
+import tensorflow as tf
+import random
+
+from vaik_pascal_voc_rw_ex import pascal_voc_rw_ex
+
+
+def int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
+def int64_list_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+
+def bytes_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+
+def bytes_list_feature(value):
+    return tf.train.Feature(bytes_list=tf.train.BytesList(value=value))
+
+
+def float_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+
+def float_list_feature(value):
+    return tf.train.Feature(float_list=tf.train.FloatList(value=value))
+
+
+def pascal_voc2tf_example(image_path, xml_path, classes, random_quality=(90, 100)):
+    tf_image = tf.image.decode_image(tf.io.read_file(image_path), channels=3)
+    encoded_image_data = tf.io.encode_jpeg(tf_image,
+                                           quality=int(random.uniform(random_quality[0], random_quality[1]))).numpy()
+    xml_dict = pascal_voc_rw_ex.read_pascal_voc_xml(xml_path)
+    width = int(xml_dict['annotation']['size']['width'])
+    height = int(xml_dict['annotation']['size']['height'])
+    if 'object' not in xml_dict['annotation'].keys():
+        object_dict_list = []
+    elif isinstance(xml_dict['annotation']['object'], list):
+        object_dict_list = xml_dict['annotation']['object']
+    else:
+        object_dict_list = [xml_dict['annotation']['object']]
+
+    filename = str.encode(os.path.basename(image_path))
+    format = tf.cast('jpeg', tf.string)
+    image_format = str.encode(format.numpy().decode('utf-8'))
+
+    x_min_array = tf.TensorArray(tf.float32, size=len(object_dict_list))
+    x_max_array = tf.TensorArray(tf.float32, size=len(object_dict_list))
+    y_min_array = tf.TensorArray(tf.float32, size=len(object_dict_list))
+    y_max_array = tf.TensorArray(tf.float32, size=len(object_dict_list))
+    class_label_array = tf.TensorArray(tf.string, size=len(object_dict_list))
+    class_index_array = tf.TensorArray(tf.int64, size=len(object_dict_list))
+
+    for index, object_dict in enumerate(object_dict_list):
+        x_min_array = x_min_array.write(index, tf.cast(int(object_dict['bndbox']['xmin']) / width, tf.float32))
+        x_max_array = x_max_array.write(index, tf.cast(int(object_dict['bndbox']['xmax']) / width, tf.float32))
+        y_min_array = y_min_array.write(index, tf.cast(int(object_dict['bndbox']['ymin']) / height, tf.float32))
+        y_max_array = y_max_array.write(index, tf.cast(int(object_dict['bndbox']['ymax']) / height, tf.float32))
+        class_label_array = class_label_array.write(index, tf.cast(object_dict['name'], tf.string))
+        class_index_array = class_index_array.write(index, tf.cast(classes.index(object_dict['name']), tf.int64))
+
+    classes_text = [str.encode(label.decode('utf-8')) for label in class_label_array.stack().numpy().tolist()]
+
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/height': int64_feature(height),
+        'image/width': int64_feature(width),
+        'image/filename': bytes_feature(filename),
+        'image/source_id': bytes_feature(filename),
+        'image/encoded': bytes_feature(encoded_image_data),
+        'image/format': bytes_feature(image_format),
+        'image/object/bbox/xmin': float_list_feature(x_min_array.stack()),
+        'image/object/bbox/xmax': float_list_feature(x_max_array.stack()),
+        'image/object/bbox/ymin': float_list_feature(y_min_array.stack()),
+        'image/object/bbox/ymax': float_list_feature(y_max_array.stack()),
+        'image/object/class/text': bytes_list_feature(classes_text),
+        'image/object/class/label': int64_list_feature(class_index_array.stack()),
+    }))
+    return tf_example
---
+++ main.py
@@ -0,0 +1,73 @@
+import argparse
+import os
+import glob
+from multiprocessing import Process, cpu_count
+import tensorflow as tf
+from tqdm import tqdm
+
+from io_tfrecords import pascal_voc2tf_example
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+def write(image_xml_path_list, output_dir_path, classes, proc_num, records_prefix_index):
+    tf_record_writer = tf.io.TFRecordWriter(os.path.join(output_dir_path, f"records-{records_prefix_index}{proc_num:03d}"))
+    for image_path, xml_path in tqdm(image_xml_path_list):
+        example = pascal_voc2tf_example(image_path, xml_path, classes)
+        tf_record_writer.write(example.SerializeToString())
+    tf_record_writer.close()
+
+def split(a_list, split_num):
+    result_list = []
+    for split_index in range(split_num):
+        result_list.append([])
+    for index, an_elem in enumerate(a_list):
+        result_list[index % split_num].append(an_elem)
+    return result_list
+
+def main(input_image_dir_path, input_classes_path, output_dir_path, records_prefix_index, cpu_count):
+    os.makedirs(output_dir_path, exist_ok=True)
+
+    classes = []
+    with open(input_classes_path) as f:
+        for line in f:
+            classes.append(line.strip())
+
+    types = ('*.jpg', '*.jpeg', '*.png', '*.JPG', '*.JPEG', '*.PNG')
+    image_path_list = []
+    for files in types:
+        image_path_list.extend(glob.glob(os.path.join(input_image_dir_path, files), recursive=True))
+
+    image_xml_path_list = []
+    for image_path in image_path_list:
+        xml_path = os.path.splitext(image_path)[0]+'.xml'
+        if os.path.exists(xml_path):
+            image_xml_path_list.append((image_path, xml_path))
+
+    split_image_xml_path_list = split(image_xml_path_list, cpu_count)
+
+    processes = []
+    for proc_num in range(len(split_image_xml_path_list)):
+        p = Process(target=write, args=(split_image_xml_path_list[proc_num], output_dir_path, classes, proc_num, records_prefix_index))
+        processes.append(p)
+
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='main')
+    parser.add_argument('--input_image_dir_path', type=str, default='~/.vaik-mnist-detection-dataset/train')
+    parser.add_argument('--input_classes_path', type=str,
+                        default='~/.vaik-mnist-detection-dataset/classes.txt')
+    parser.add_argument('--output_dir_path', type=str,
+                        default='~/.vaik-pascalvoc2tfrecord-mp/vaik-mnist-detection-dataset/train')
+    parser.add_argument('--records_prefix_index', type=str, default='00')
+    parser.add_argument('--cpu_count', type=int, default=cpu_count())
+    args = parser.parse_args()
+
+    args.input_image_dir_path = os.path.expanduser(args.input_image_dir_path)
+    args.input_classes_path = os.path.expanduser(args.input_classes_path)
+    args.output_dir_path = os.path.expanduser(args.output_dir_path)
+
+    main(**args.__dict__)
